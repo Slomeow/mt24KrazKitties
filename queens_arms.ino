ec946a024f378e7760b6e67719151a2e69f98801
Anoushka <165952351+achig31@users.noreply.github.com>
2024-04-16 17:05:20 -0500

    queens_arms.ino

--- queens_arms.ino
+++ queens_arms.ino
@@ -1,27 +1,23 @@
 #include <Servo.h>
 
-Servo myservo;  //queen's arms
-
-//either side of the servo is attached to the queens arms and they move up and down
-
-const int buttonPin = 2; 
-int buttonVal;
-int pos = 0;
+Servo queenarm; // queen's arms moving 
+int servoPin = 9; 
+int buttonPin = 2; 
 
 void setup() {
-  myservo.attach(9);  
-  pinMode(buttonPin, INPUT);
+ queenarm.attach(servoPin); 
+ pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  if(digitalRead(buttonPin) == LOW){
-   pos = 70;
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);             
-   
-    }else{
-      pos = 110;
-       delay(15);
-      myservo.write(pos);  
-  }
+ if (digitalRead(buttonPin) == HIGH) { 
+    for (int pos = 65; pos <= 115; pos += 1) { 
+      queenarm.write(pos);
+      delay(15); 
+    }
+    for (int pos = 115; pos >= 65; pos -= 1) { 
+      queenarm.write(pos); 
+      delay(15); 
+    }
+ }
 }
